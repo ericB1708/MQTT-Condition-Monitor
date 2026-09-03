25c1101c57ba1dca344f47c979bc136bbba44ac9
ericB1708 <eric.berndt2001@googlemail.com>
2026-09-03 17:11:25 +0200

    docs: add secrets_template.h for easy setup

---
+++ espSensorDataMqtt/IIoT_Predictive_Maintenance/include/secrets_template.h
@@ -0,0 +1,14 @@
+#pragma once
+
+// ================================================================
+// WICHTIG:
+// 1. Benenne diese Datei in "secrets.h" um.
+// 2. Trage unten deine echten WLAN- und MQTT-Daten ein.
+// ================================================================
+
+// WLAN
+#define SECRET_WIFI_SSID "YOUR_WIFI_SSID_HERE"
+#define SECRET_WIFI_PASS "YOUR_WIFI_PASSWORD_HERE"
+
+// MQTT
+#define SECRET_MQTT_BROKER "IP" // Trage hier die IP deines Brokers ein
